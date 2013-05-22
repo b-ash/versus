4603d0ac4374e9942aa96f8bef36e04e62026d3f
Bash Ash <bash@hubspot.com>
2013-05-22 10:35:32 -0400

    Changing urls and removing nodejitsu mention

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -61,20 +61,25 @@
     throw new Error('Cannot find module "' + name + '"');
   };
 
-  var define = function(bundle) {
-    for (var key in bundle) {
-      if (has(bundle, key)) {
-        modules[key] = bundle[key];
+  var define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has(bundle, key)) {
+          modules[key] = bundle[key];
+        }
       }
+    } else {
+      modules[bundle] = fn;
     }
-  }
+  };
 
   globals.require = require;
   globals.require.define = define;
+  globals.require.register = define;
   globals.require.brunch = true;
 })();
 
-window.require.define({"application": function(exports, require, module) {
+window.require.register("application", function(exports, require, module) {
   var Application;
 
   Application = {
@@ -92,9 +97,8 @@ window.require.define({"application": function(exports, require, module) {
 
   module.exports = Application;
   
-}});
-
-window.require.define({"collections/battles": function(exports, require, module) {
+});
+window.require.register("collections/battles", function(exports, require, module) {
   var Battle, Battles,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
@@ -119,9 +123,8 @@ window.require.define({"collections/battles": function(exports, require, module)
 
   module.exports = Battles;
   
-}});
-
-window.require.define({"initialize": function(exports, require, module) {
+});
+window.require.register("initialize", function(exports, require, module) {
   
   window.app = require('application');
 
@@ -129,9 +132,8 @@ window.require.define({"initialize": function(exports, require, module) {
     return app.initialize(function() {});
   });
   
-}});
-
-window.require.define({"lib/router": function(exports, require, module) {
+});
+window.require.register("lib/router", function(exports, require, module) {
   var IndexView, Router, app,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
@@ -162,9 +164,8 @@ window.require.define({"lib/router": function(exports, require, module) {
 
   })(Backbone.Router);
   
-}});
-
-window.require.define({"lib/view_helper": function(exports, require, module) {
+});
+window.require.register("lib/view_helper", function(exports, require, module) {
   
   Handlebars.registerHelper("debug", function(optionalValue) {
     console.log("Current Context");
@@ -254,9 +255,8 @@ window.require.define({"lib/view_helper": function(exports, require, module) {
     return buffer;
   });
   
-}});
-
-window.require.define({"models/battle": function(exports, require, module) {
+});
+window.require.register("models/battle", function(exports, require, module) {
   var Backbone, Battle,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
@@ -298,9 +298,8 @@ window.require.define({"models/battle": function(exports, require, module) {
 
   module.exports = Battle;
   
-}});
-
-window.require.define({"views/index": function(exports, require, module) {
+});
+window.require.register("views/index", function(exports, require, module) {
   var BattleView, Battles, IndexView, View,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
@@ -528,9 +527,8 @@ window.require.define({"views/index": function(exports, require, module) {
 
   module.exports = IndexView;
   
-}});
-
-window.require.define({"views/templates/battle": function(exports, require, module) {
+});
+window.require.register("views/templates/battle", function(exports, require, module) {
   module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
     helpers = helpers || Handlebars.helpers;
     var buffer = "", stack1, stack2, stack3, foundHelper, tmp1, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression, blockHelperMissing=helpers.blockHelperMissing;
@@ -707,18 +705,16 @@ window.require.define({"views/templates/battle": function(exports, require, modu
     if(stack1 || stack1 === 0) { buffer += stack1; }
     buffer += "\n    <div class=\"clear\"></div>\n</div>\n";
     return buffer;});
-}});
-
-window.require.define({"views/templates/index": function(exports, require, module) {
+});
+window.require.register("views/templates/index", function(exports, require, module) {
   module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
     helpers = helpers || Handlebars.helpers;
     var foundHelper, self=this;
 
 
-    return "<div id=\"header-wrap\">\n    <div id=\"header\"></div>\n    <div id=\"header-graphic-wrap\">\n        <div id=\"header-graphic\"></div>\n    </div>\n</div>\n\n<div id=\"index-view\">\n    <div id=\"list\"></div>\n</div>\n\n<div id=\"footer-wrap\">\n    <div id=\"footer\">\n        <span id=\"tagline\">Made with &hearts; and humor</span>\n        <span class=\"inline-divider\">|</span>\n        <span id=\"about\">Who Are We?</span>\n        <span class=\"inline-divider\">|</span>\n        <span id=\"contact\">Contact Us</span>\n        <span class=\"inline-divider\">|</span>\n        <span id=\"copyright\">&copy; 2012</span>\n    </div>\n</div>\n\n<div id=\"footer-panels\">\n    <div id=\"about-panel\">\n        <div id=\"about-content\">\n            <div id=\"about-img-wrap\" class=\"left\">\n                <img src=\"/static/images/about.jpg\" />\n            </div>\n            <div id=\"about-txt-wrap\" class=\"left\">\n                <span class=\"title\">The Crew</span>\n                <p>Jayeth and Basheus are just two dudes versus the world. In Boston, we have Basheus: a debonair, industrious code monkey who loves all things tech, fitness and music. In Chicago resides Jayeth: imaginative, dexterous and graphics-savvy, spreading visuals and musical prowess to the masses.</p>\n\n                <span class=\"title\">The Task</span>\n                <p>Conquering Boston and Chicago is just one feat on their list. Long-time friends and bandmates, these two have seen many things. Why not rate and assess really minscule, unnecessary, potentially uninteresting things? The world is their oyster.<p>\n\n                <p>Gauntlet thrown; gauntlet picked up.</p>\n            </div>\n        </div>\n    </div>\n    <div id=\"contact-panel\">\n        <div id=\"contact-content\">\n            <div id=\"contact-jay\" class=\"left\">\n                <span class=\"name\">Jay</span>\n\n                <a href=\"https://twitter.com/JaySpiwak\" class=\"twitter-follow-button\" data-show-count=\"false\" data-show-screen-name=\"false\">Follow @JaySpiwak</a>\n                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"//platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>\n\n                <a class=\"personal-site\" target=\"_blank\" href=\"http://www.jspiwak.com/\">www.jspiwak.com</a>\n            </div>\n            <div id=\"contact-bash\" class=\"left\">\n                <span class=\"name\">Bash</span>\n\n                <a href=\"https://twitter.com/Bryan_Ash\" class=\"twitter-follow-button\" data-show-count=\"false\" data-show-screen-name=\"false\">Follow @Bryan_Ash</a>\n                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"//platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>\n\n                <a class=\"personal-site\" target=\"_blank\" href=\"https://github.com/b-ash\">github</a>\n            </div>\n            <div class=\"clear\"></div>\n            <div id=\"contact-xtra\">\n                This page is powered by <a target=\"_blank\" href=\"http://www.nodejitsu.com\">Nodejitsu</a>. Check out the <a target=\"_blank\" href=\"https://github.com/b-ash/versus\">code</a>.\n            </div>\n        </div>\n    </div>\n</div>\n";});
-}});
-
-window.require.define({"views/view": function(exports, require, module) {
+    return "<div id=\"header-wrap\">\n    <div id=\"header\"></div>\n    <div id=\"header-graphic-wrap\">\n        <div id=\"header-graphic\"></div>\n    </div>\n</div>\n\n<div id=\"index-view\">\n    <div id=\"list\"></div>\n</div>\n\n<div id=\"footer-wrap\">\n    <div id=\"footer\">\n        <span id=\"tagline\">Made with &hearts; and humor</span>\n        <span class=\"inline-divider\">|</span>\n        <span id=\"about\">Who Are We?</span>\n        <span class=\"inline-divider\">|</span>\n        <span id=\"contact\">Contact Us</span>\n        <span class=\"inline-divider\">|</span>\n        <span id=\"copyright\">&copy; 2012</span>\n    </div>\n</div>\n\n<div id=\"footer-panels\">\n    <div id=\"about-panel\">\n        <div id=\"about-content\">\n            <div id=\"about-img-wrap\" class=\"left\">\n                <img src=\"/static/images/about.jpg\" />\n            </div>\n            <div id=\"about-txt-wrap\" class=\"left\">\n                <span class=\"title\">The Crew</span>\n                <p>Jayeth and Basheus are just two dudes versus the world. In Boston, we have Basheus: a debonair, industrious code monkey who loves all things tech, fitness and music. In Chicago resides Jayeth: imaginative, dexterous and graphics-savvy, spreading visuals and musical prowess to the masses.</p>\n\n                <span class=\"title\">The Task</span>\n                <p>Conquering Boston and Chicago is just one feat on their list. Long-time friends and bandmates, these two have seen many things. Why not rate and assess really minscule, unnecessary, potentially uninteresting things? The world is their oyster.<p>\n\n                <p>Gauntlet thrown; gauntlet picked up.</p>\n            </div>\n        </div>\n    </div>\n    <div id=\"contact-panel\">\n        <div id=\"contact-content\">\n            <div id=\"contact-jay\" class=\"left\">\n                <span class=\"name\">Jay</span>\n\n                <a href=\"https://twitter.com/JaySpiwak\" class=\"twitter-follow-button\" data-show-count=\"false\" data-show-screen-name=\"false\">Follow @JaySpiwak</a>\n                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"//platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>\n\n                <a class=\"personal-site\" target=\"_blank\" href=\"http://www.jspiwak.com/\">www.jspiwak.com</a>\n            </div>\n            <div id=\"contact-bash\" class=\"left\">\n                <span class=\"name\">Bash</span>\n\n                <a href=\"https://twitter.com/Bryan_Ash\" class=\"twitter-follow-button\" data-show-count=\"false\" data-show-screen-name=\"false\">Follow @Bryan_Ash</a>\n                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"//platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>\n\n                <a class=\"personal-site\" target=\"_blank\" href=\"http://bry.io\">bry.io</a>\n            </div>\n        </div>\n    </div>\n</div>\n";});
+});
+window.require.register("views/view", function(exports, require, module) {
   var View, app,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
@@ -793,5 +789,4 @@ window.require.define({"views/view": function(exports, require, module) {
 
   })(Backbone.View);
   
-}});
-
+});
